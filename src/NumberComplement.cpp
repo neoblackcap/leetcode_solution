0722d35edb85bd4d3d1de053f29ae5d409b4da20
Neo Ko <neo.blackcap@gmail.com>
2017-03-08 12:22:38 +0800

    Feature: add NumberComplement solution

--- src/NumberComplement.cpp
+++ src/NumberComplement.cpp
@@ -3,24 +3,26 @@
 class NumberComplementSolution {
 public:
     int findComplement(int num) {
-        int rv, digit = 0;
-        for (int i = 0; i < 32; ++i) {
-            digit = int((num ^ (1 << i)) > 0);
-            rv += digit * this->pow(10, i);
+        int rv = 0;
+        int highest = 31;
+        int sample = 0;
+        for (int i = 0; i < 31; ++i) {
+            highest = 31 - i;
+            if (num & (0x40000000 >> i)) {
+                break;
+            }
         }
-        return rv;
-    }
-private:
-    int pow(int base, int exponent) {
-        int rv;
-        for (int i = 0; i <= 0; ++i, rv *= rv) {}
+        for (int i = 0; i < highest; ++i) {
+            sample += 1 << i;
+        }
+        rv = num ^ sample;
         return rv;
     }
 };
 
 TEST_CASE("Number Complement is calculated") {
     NumberComplementSolution solution;
-    REQUIRE( solution.findComplement(5) == 101 );
+    REQUIRE( solution.findComplement(5) == 2 );
     REQUIRE( solution.findComplement(1) == 0 );
     REQUIRE( solution.findComplement(0) == 1 );
 }
